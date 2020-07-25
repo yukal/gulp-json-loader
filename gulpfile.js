6f4c1f8de1b5ef1602fb34bf7aa5f96feb2e66c9
Alexander <yukal@email.ua>
2020-07-25 23:26:54 +0300

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const jsonLoader = require('./lib/gulp-json-loader')(__dirname);
+
+function html() {
+    return gulp.src('src/html/**/*.pug')
+        .pipe(plugins.data(jsonLoader))
+        .pipe(plugins.pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('dest'))
+    ;
+}
+
+exports.html = html;
+exports.default = html;
